dd8171fcd30b5c8874aec880325acf73bd1f0a5d
Farzad Rezaei <fr355@exeter.ac.uk>
2025-07-21 16:44:38 +0100

    enum and enum class

---
+++ C++taturial/enums.cpp
@@ -0,0 +1,62 @@
+//
+//  enums.cpp
+//  C++taturial
+//
+//  Created by Kasra Rezaei on 21/07/2025.
+//
+/////////////////// enum
+//enum shirtColor
+//{
+//    Red = 1,
+//    Blue = 2,
+//    Yellow =3
+//};
+//
+//int main()
+//{
+//    shirtColor shirtColor {shirtColor::Yellow};
+//
+//    if (shirtColor == 1)
+//    {
+//        std::cout <<"Shirt color is red"<< std::endl;
+//    } else if (shirtColor == 2)
+//    {
+//        std::cout <<"Shirt color is blue"<< std::endl;
+//    } else
+//    {
+//        std::cout <<"Shirt color is yellow"<< std::endl;
+//
+//    }
+//
+//}
+/////////////////
+
+
+/// enum class
+//does not support int assignment to the enum state
+//enum class shirtColor
+//{
+//    Red,
+//    Blue,
+//    Yellow
+//};
+//
+//int main()
+//{
+//    shirtColor shirtColor {shirtColor::Yellow};
+//
+//    if (shirtColor == shirtColor::Red)
+//    {
+//        std::cout <<"Shirt color is red"<< std::endl;
+//    } else if (shirtColor == shirtColor::Blue)
+//    {
+//        std::cout <<"Shirt color is blue"<< std::endl;
+//    } else
+//    {
+//        std::cout <<"Shirt color is yellow"<< std::endl;
+//
+//    }
+//}
+////////////////
+
+
--- C++taturial/vectors.cpp
+++ C++taturial/vectors.cpp
@@ -8,28 +8,28 @@
 #include <iostream>
 #include <vector>
 
-void addOne (std::vector<int>& vec) /// & refrencing the input vector othrwise dose not change the original vector
-{
-    for (int i = 0; i < vec.size(); i++)
-    {
-        vec[i]++;
-    }
-}
-
-int main()
-{
-    std::vector<std::vector<int>> myVec;
-    
-    myVec.push_back(std::vector<int>{10, 50});
-    myVec.push_back(std::vector<int> {20, 30});
-    
-    std::cout << "size of vector: " << myVec.size() << std::endl;
-
-    for (int i = 0; i < myVec.size(); i++)
-    {
-        for (int j = 0; j < myVec.size(); j++)
-        {
-            std:: cout << myVec[i][j] << std::endl;
-        }
-    }
-}
+//void addOne (std::vector<int>& vec) /// & refrencing the input vector othrwise dose not change the original vector
+//{
+//    for (int i = 0; i < vec.size(); i++)
+//    {
+//        vec[i]++;
+//    }
+//}
+//
+//int main()
+//{
+//    std::vector<std::vector<int>> myVec;
+//    
+//    myVec.push_back(std::vector<int>{10, 50});
+//    myVec.push_back(std::vector<int> {20, 30});
+//    
+//    std::cout << "size of vector: " << myVec.size() << std::endl;
+//
+//    for (int i = 0; i < myVec.size(); i++)
+//    {
+//        for (int j = 0; j < myVec.size(); j++)
+//        {
+//            std:: cout << myVec[i][j] << std::endl;
+//        }
+//    }
+//}
