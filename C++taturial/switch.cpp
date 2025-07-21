5b435a366d4a1cc1dbb09f6faa94a75621d2f1be
Farzad Rezaei <fr355@exeter.ac.uk>
2025-07-21 17:04:20 +0100

    Switch

---
+++ C++taturial/switch.cpp
@@ -0,0 +1,41 @@
+//
+//  swithc.h
+//  C++taturial
+//
+//  Created by Kasra Rezaei on 21/07/2025.
+//
+
+//enum class GameState
+//{
+//    Menu,
+//    Playing,
+//    GameOver
+//};
+//
+//int main()
+//{
+//    GameState gamestate {GameState::Menu};
+//    
+//    switch (gamestate)
+//    {
+//        case GameState::Menu:
+//            // Show menu
+//            // load player
+//            break;
+//        
+//        case GameState::Playing:
+//            // Create player
+//            // Create enemies
+//            // load playing logic
+//            break;
+//        
+//        case GameState::GameOver:
+//            // Display game over screen
+//            // option to reload
+//            
+//        default:
+//            // Show menu screen
+//            // Display error message
+//            break;
+//    }
+//}
