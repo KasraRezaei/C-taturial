5a504b3d94dec98ce8303c32acaa1daed0fd29e7
Farzad Rezaei <fr355@exeter.ac.uk>
2025-07-21 14:55:09 +0100

    Pointer - stag all changes

--- C++taturial/main.cpp
+++ C++taturial/main.cpp
@@ -7,31 +7,4 @@
 
 #include <iostream>
 
-class AudioTrack
-{
-    // representing audio track
-};
 
-//pointing
-void analaseTrack (AudioTrack* track){ // we must dclearee if the pointer is empty
-    
-    if (track != nullptr)
-    {
-        //figure out the level
-    }
-}
-
-
-int main()
-{
-    // user chooses a song
-    // old method of creataing a dynamic object > dosen't excist at the start of program
-    //    AudioTrack* track = new AudioTrack;
-    /// decleration      //allocation
-
-    // new mwthod
-    std::unique_ptr<AudioTrack> track = std::make_unique<AudioTrack>();
-    /// decleration                                               allocation
-    analaseTrack(track.get());
-    
-}
---
+++ C++taturial/pointer.cpp
@@ -0,0 +1,37 @@
+//
+//  pointer.cpp
+//  C++taturial
+//
+//  Created by Kasra Rezaei on 21/07/2025.
+//
+
+#include <iostream>
+
+class AudioTrack
+{
+    // representing audio track
+};
+
+//pointing
+void analaseTrack (AudioTrack* track){ // we must dclearee if the pointer is empty
+    
+    if (track != nullptr)
+    {
+        //figure out the level
+    }
+}
+
+
+int main()
+{
+    // user chooses a song
+    // old method of creataing a dynamic object > dosen't excist at the start of program
+    //    AudioTrack* track = new AudioTrack;
+    /// decleration      //allocation
+
+    // new mwthod
+    std::unique_ptr<AudioTrack> track = std::make_unique<AudioTrack>();
+    /// decleration                                               allocation
+    analaseTrack(track.get());
+    
+}
