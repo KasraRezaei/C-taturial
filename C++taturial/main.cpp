8f7b6c233b87c843e246fced562c833518c9e4ec
Farzad Rezaei <fr355@exeter.ac.uk>
2025-08-12 12:16:58 +0100

    Assert

--- C++taturial/main.cpp
+++ C++taturial/main.cpp
@@ -6,8 +6,12 @@
 //
 #include <iostream>
 #include "VirtualFunction.hpp"
+#include <assert.h> //used for debugging
 int main()
 {
+    int a = 10;
+    assert(a !=10); // is A Not equal 10?? if not brakes
+    
     Animal animal;
     animal.move();
     
@@ -20,5 +24,7 @@ int main()
     fishNo1 = std::make_unique<Fish>(); //create a fish object and pass to pointer
     doSomthing(fishNo1.get()); // call the pointer
     fishNo1->move();
+    
+    
 }
 
