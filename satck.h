6b6892b70e1481589e12752e0ef6a6f0b620eb1f
Farzad Rezaei <fr355@exeter.ac.uk>
2026-03-02 12:02:39 +0000

    Creating c structure files

---
+++ satck.h
@@ -0,0 +1,20 @@
+/* • stack.h
+• #include-s any .h files needed by stack.c
+• defines structures, typedefs etc
+• defines function prototypes (push, pop, printStack, emptyStack)
+• is #include-ed into any program that wants to use the stack functionality
+*/
+
+/* structure definitions */
+
+struct stactStruct{
+    char data;
+    struct stactStruct *next;
+}
+typedef struct stactStruct STACK;
+
+/* functions prototypes */
+extern STACK *puch(STACK *, char);
+extern char *pop(STACK **);
+extern void print(STACK *);
+extern void emptyStack(STACK **);
---
+++ stack.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "stack.h"
+
+int main(){
+    STACK *stack = NULL;
+
+    printStack(stack);
+    stack = push(stack, "Hello");
+    printStack(stack);
+    stack = push(stack, "World");
+    printStack(stack);
+
+    char c = pop(stack);
+
+    emptyStack(stack);
+    return 0;
+}
